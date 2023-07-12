fa8ce8a192fce3db0cb9a7dc2b60f0706ff0ffcf
MP <mp@swoop.pro>
2023-07-12 11:54:19 +0300

    task 18 done - input square output range

---
+++ seminar/task18/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please input your square");
+int square = Convert.ToInt32(Console.ReadLine());
+if (square == 1) Console.WriteLine("X > 0; Y > 0");
+else if (square == 2) Console.WriteLine("X < 0; Y > 0");
+else if (square == 3) Console.WriteLine("X < 0; Y < 0");
+else if (square == 4) Console.WriteLine("X > 0; Y < 0");
+else Console.WriteLine("input error");
