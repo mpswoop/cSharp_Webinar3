1294c88ea6d0d5a5689843f67d41244e9be69fa3
MP <mp@swoop.pro>
2023-07-12 11:44:34 +0300

    task 17 done - input coordinates & show square

---
+++ seminar/task17/Program.cs
@@ -0,0 +1,37 @@
+﻿// input coordinates X & Y both != 0 and show sqare of coordinates to be demonstrated
+Console.WriteLine("input X coordinate");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input Y coordinate");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 || y == 0) 
+{
+    Console.WriteLine("Input 0 not accepted");
+}
+
+// else if (y == 0)
+// {
+//     Console.WriteLine("Input 0 not accepted");
+// }
+
+else
+
+    if (x > 1 && y > 1)
+    {
+        Console.WriteLine("your dot is in square 1");
+    }
+
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("your dot is in square 2");
+    }
+
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("your dot is in square 3");
+    }
+
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("your dot is in square 4");
+    }
