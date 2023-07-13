89e57c629b591714bc3f0282a07d0fbdf10a2471
MP <mp@swoop.pro>
2023-07-13 12:42:25 +0300

    task 23 completed - row of cubes

---
+++ homework/task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// input number N, output table of cubes from 1 to N
+Console.Clear();
+Console.WriteLine("Please input a number");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+num1 = Math.Abs(num1);
+
+int i = 1;
+
+Console.WriteLine(""); 
+
+if (num1 == 0)
+{
+    Console.WriteLine("input 0 is not valid");
+    
+}
+
+else 
+
+Console.WriteLine("row of squares between 1 and input"); 
+
+while (i <= num1)
+{
+    Console.WriteLine(Math.Pow(i,3));
+    i++;
+}
